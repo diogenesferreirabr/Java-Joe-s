fc6c1b572da0332d69bcc1be1f97e4aaa6cbc955
Diogenes Ferreira <95397773+diogenesferreirabr@users.noreply.github.com>
2025-06-30 21:27:22 -0300

    adc javascript

---
+++ index.js
@@ -0,0 +1,9 @@
+//alerta de boas vindas
+alert("Sejam Bem Vindos ao Java Joe's!");
+
+// declarar variavel data 
+let data_atual = new Date();
+
+// impremir a variavel data em uma div html
+
+document.getElementById("tempo").innerText = data_atual;
